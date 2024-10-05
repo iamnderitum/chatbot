aeed2a206dea340ba0811e56c0d534f811e5af66
Moses Ndungu <moseswambui044@gmail.com>
2024-10-05 04:01:35 +0300

    Added Userquery model, serializer and view

--- chatbot_service/models.py
+++ chatbot_service/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class UserQuery(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    query = models.TextField()
+    response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Query by {self.user.username} on {self.timestamp}"
